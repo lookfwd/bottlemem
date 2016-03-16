3f93a9f27b49a78e658deeb284573f279ea2b194
Ubuntu <ubuntu@ip-10-84-27-94.eu-west-1.compute.internal>
2016-03-16 04:33:21 +0000

    updates

--- 1.simple_server.py
+++ 1.simple_server.py
@@ -4,16 +4,16 @@ import socket
 from time import sleep
 
 APP_PORT = 50000
-LISTEN_BACKLOG = 100000
+LISTEN_BACKLOG = 1000000
 
 LOGIN_SERVER = '52.17.32.15'
 LOGIN_PORT = 50001
 
-def login(data):
+def login():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((LOGIN_SERVER, LOGIN_PORT))
 
-    s.sendall(('login-%d' % data).encode())
+    s.sendall(('login').encode())
     data = s.recv(1024)
 
     s.close()
@@ -26,14 +26,39 @@ def initListening():
     s.listen(LISTEN_BACKLOG)
     return s
 
+SECURITIES = 10000
 
-def business_logic(data):
-    login(data)
+import threading
+lock = threading.RLock()
+import numpy as np
+accounts = [np.random.randint(0, 1000, SECURITIES) for i in range(10)]
+
+def manage_account(account, action):
+    group, buy, quantity, id = action
+
+    if (not buy) and quantity > account[id]:
+        return "can't sell"
+
+    if buy:
+        account[id] += quantity
+    else:
+        account[id] -= quantity
 
     return "ok"
 
+
+def business_logic(data):
+    action = tuple(map(int, data.split()))
+
+    login()
+
+    group = action[0]
+
+    with lock:
+        return manage_account(accounts[group], action)
+
 def serve(conn):
-    request = int(conn.recv(1024))
+    request = conn.recv(1024)
 
     resp = business_logic(request)
 
@@ -45,7 +70,7 @@ s = initListening()
 
 import concurrent 
 from concurrent.futures import ThreadPoolExecutor
-with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
     while True:
         conn, addr = s.accept()
         #serve(conn)
